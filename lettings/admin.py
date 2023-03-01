d10b7d67154b2cf835726148f8caf4a39938e38e
matbl <matblanchard.pro@gmail.com>
2023-03-01 17:41:30 +0100

    Mis à jour flake8

--- lettings/admin.py
+++ lettings/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
 from lettings.models import Letting, Address
 
-class AdressAdmin(admin.ModelAdmin):
-    list_display = ['id',
-                    'number']
-
 admin.site.register(Letting)
-admin.site.register(Address, AdressAdmin)
+admin.site.register(Address)
--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
+from lettings.models import Letting, Address
+from pytest_django.asserts import assertTemplateUsed
+import pytest
 
-# Create your tests here.
+
+class TestLettingApp(TestCase):
+    client = Client()
+
+    @pytest.mark.django_db
+    def setUp(self):
+        address = Address.objects.create(
+            number=99,
+            street='Avenue des Champs-Élysées',
+            city='Paris',
+            state='Paris',
+            zip_code=75008,
+            country_iso_code='FRA'
+        )
+        Letting.objects.create(
+            title='Fouquets',
+            address=address
+        )
+
+    @pytest.mark.django_db
+    def test_lettings_index_view(self):
+        path = reverse('lettings:index')
+        response = self.client.get(path)
+        content = response.content.decode()
+        assert response.status_code == 200
+        assert Letting.objects.all().first().title in content
+        assertTemplateUsed(response, 'lettings/index.html')
+
+    @pytest.mark.django_db
+    def test_letting_detail_view(self):
+        letting = Letting.objects.all().first()
+        path = reverse('lettings:letting', kwargs={'letting_id': letting.id})
+        response = self.client.get(path)
+        content = response.content.decode()
+        assert response.status_code == 200
+        assert letting.title in content
+        assertTemplateUsed(response, 'lettings/letting.html')
