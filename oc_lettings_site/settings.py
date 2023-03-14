a878018db9af6829a33bfe4afd0f1e0c4c7360a1
MatBlanchard <matblanchard.pro@gmail.com>
2023-03-14 15:50:54 +0100

    Test sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -117,15 +117,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 sentry_sdk.init(
-    dsn=os.getenv('https://0afd3ed15a8a454e9f4283d083d255ab@o4504837327159296.ingest.sentry.io/4504837465112576'),
+    dsn="https://0afd3ed15a8a454e9f4283d083d255ab@o4504837327159296.ingest.sentry.io/4504837465112576",
     integrations=[DjangoIntegration()],
 
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
     traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
 )
