c1943968164e3d8417e87aab41e188f1c2b71b74
MatBlanchard <matblanchard.pro@gmail.com>
2023-03-14 15:13:34 +0100

    Test sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['oclettings-site.herokuapp.com', '0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['oclettings-site.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
